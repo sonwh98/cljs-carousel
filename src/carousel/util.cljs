c33eb10d0b59940b1dcce73f9f8ff49c6dc62d8f
sto <Son.C.To@gmail.com>
2015-07-15 12:09:28 +0200

    events->chan takes a node (vector) dsl as well

--- src/carousel/util.cljs
+++ src/carousel/util.cljs
@@ -1,19 +1,22 @@
 (ns ^:figwheel-always carousel.util
     (:require [cljs.core.async :refer [put! chan] ]
-               [com.famous.Famous]))
+              [com.famous.Famous]))
 
 (defonce famous js/famous)
 (defonce GestureHandler (.. famous -components -GestureHandler))
 
 (defn events->chan
-      "Given a target DOM element and event type return a channel of
-      observed events. Can supply the channel to receive events as third
-      optional argument."
-      ([node event] (events->chan node event (chan)))
-      ([node event c]
-        (.. (GestureHandler. node) (on event (fn []
-                                                 (put! c event))))
-        c))
+  "Given a node and event type return a channel of
+  observed events. Can supply the channel to receive events as third
+  optional argument."
+  ([node event] (events->chan node event (chan)))
+  ([node event c]
+   (let [node (if (= (type node) PersistentVector)
+                (-> node meta :node)
+                node)]
+    (.. (GestureHandler. node) (on event (fn []
+                                           (put! c event)))))
+   c))
 
 (defmulti get-children
   (fn [v]
